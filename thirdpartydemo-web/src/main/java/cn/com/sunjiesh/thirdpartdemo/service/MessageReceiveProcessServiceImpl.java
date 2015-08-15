c1993655ded341ceac8676a5301a3b804b4de2cc
tomsun <sunjie310110@gmail.com>
2015-08-15 18:30:08 +0800

    增加测试消息接受Demo

--- thirdpartydemo-web/src/main/java/cn/com/sunjiesh/thirdpartdemo/service/MessageReceiveProcessServiceImpl.java
+++ thirdpartydemo-web/src/main/java/cn/com/sunjiesh/thirdpartdemo/service/MessageReceiveProcessServiceImpl.java
@@ -1,33 +1,22 @@
-package cn.com.sunjiesh.thirdpartdemo.web;
-
-import javax.servlet.http.HttpServletResponse;
+package cn.com.sunjiesh.thirdpartdemo.service;
 
 import org.dom4j.Document;
 import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-import cn.com.sunjiesh.thirdpartdemo.entity.WechatMessage;
-import cn.com.sunjiesh.utils.web.SpringBaseController;
+import cn.com.sunjiesh.wechat.entity.WechatMessage;
+import cn.com.sunjiesh.wechat.service.IMessageReceiveProcessService;
+import cn.com.sunjiesh.xcutils.common.base.ServiceException;
 
-@Controller
-@RequestMapping("/wechat/message")
-public class WechatMessageController extends SpringBaseController{
+public class MessageReceiveProcessServiceImpl  implements IMessageReceiveProcessService{
 
 	private Logger logger=LoggerFactory.getLogger(getClass());
 	
-	/**
-	 * 问题消息接收处理
-	 * @param message
-	 * @param response
-	 */
-	@RequestMapping(value = "/textMessageRecive.do", method = RequestMethod.POST)
-	public void textMessageRecive(@RequestBody final WechatMessage message,HttpServletResponse response){
+	@Override
+	public Document messageRecive(WechatMessage wechatMessage)
+			throws ServiceException {
 		logger.debug("receive a textMessageRecive request ");
 		Document doc4j=DocumentHelper.createDocument();
 		Element rootEle=doc4j.addElement("xml");
@@ -41,7 +30,7 @@ public class WechatMessageController extends SpringBaseController{
 		createTimeEle.setText(String.valueOf(System.currentTimeMillis()));
 		msgTypeEle.setText("text");
 		contentEle.setText("你好");
-		String xml=doc4j.asXML();
-		super.responseXml(response, xml);
+		return doc4j;
 	}
+
 }
