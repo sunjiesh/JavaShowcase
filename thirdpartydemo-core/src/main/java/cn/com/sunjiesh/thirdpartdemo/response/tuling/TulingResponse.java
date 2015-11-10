d6315a7ac8a72e3140dd180a3844fb7cd368fc50
TomSun <sunjie310110@gmail.com>
2015-11-10 21:24:48 +0800

    添加Tuling返回链接类模擬

--- thirdpartydemo-core/src/main/java/cn/com/sunjiesh/thirdpartdemo/response/tuling/TulingResponse.java
+++ thirdpartydemo-core/src/main/java/cn/com/sunjiesh/thirdpartdemo/response/tuling/TulingResponse.java
@@ -12,7 +12,7 @@ package cn.com.sunjiesh.thirdpartdemo.response.tuling;
 public class TulingResponse {
     
     /**
-     * 文字类标识码
+     * 标识码
      */
     private Integer code;
     
@@ -20,6 +20,19 @@ public class TulingResponse {
      * 结果
      */
     private String text;
+    
+    /**
+     * 链接地址
+     */
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 
     public Integer getCode() {
         return code;
