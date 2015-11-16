e1bc4745a926ef2b0e230af3d98a2fda081210d4
TomSun <sunjie310110@gmail.com>
2015-11-16 22:03:33 +0800

    對接tuling123返回鏈接處理

--- thirdpartydemo-web/src/main/java/cn/com/sunjiesh/thirdpartdemo/service/CustomerWechatMessageReceiveProcessServiceImpl.java
+++ thirdpartydemo-web/src/main/java/cn/com/sunjiesh/thirdpartdemo/service/CustomerWechatMessageReceiveProcessServiceImpl.java
@@ -13,13 +13,21 @@ import cn.com.sunjiesh.thirdpartdemo.helper.tuling123.TulingConstants;
 import cn.com.sunjiesh.thirdpartdemo.helper.tuling123.TulingHelper;
 import cn.com.sunjiesh.thirdpartdemo.model.WechatUser;
 import cn.com.sunjiesh.thirdpartdemo.response.tuling.TulingResponse;
+import cn.com.sunjiesh.utils.thirdparty.base.HttpService;
 import cn.com.sunjiesh.wechat.entity.message.event.WechatReceiveEventSubscribeMessage;
 import cn.com.sunjiesh.wechat.entity.message.event.WechatReceiveEventUnSubscribeMessage;
+import cn.com.sunjiesh.wechat.handler.WechatMediaHandler;
 import cn.com.sunjiesh.wechat.model.dto.user.WechatUserDto;
 import cn.com.sunjiesh.wechat.model.request.message.WechatNormalTextMessageRequest;
+import cn.com.sunjiesh.wechat.model.response.media.WechatUploadMediaResponse;
+import cn.com.sunjiesh.wechat.service.WechatMediaService;
 import cn.com.sunjiesh.wechat.service.WechatMessageReceiveProcessServiceImpl;
 import cn.com.sunjiesh.wechat.service.WechatUserService;
 import cn.com.sunjiesh.xcutils.common.base.ServiceException;
+import java.io.File;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.logging.Level;
 
 public class CustomerWechatMessageReceiveProcessServiceImpl extends WechatMessageReceiveProcessServiceImpl {
 
@@ -28,6 +36,10 @@ public class CustomerWechatMessageReceiveProcessServiceImpl extends WechatMessag
     private ThirdpartyUserService thirdpartyUserService;
 
     private WechatUserService wechatUserService;
+
+    private WechatMediaService wechatMediaService;
+
+    private ScheduledExecutorService scheduledThreadPool = Executors.newScheduledThreadPool(5);
 
     @Override
     public Document messageRecive(WechatReceiveEventSubscribeMessage wechatMessage) throws ServiceException {
@@ -70,7 +82,7 @@ public class CustomerWechatMessageReceiveProcessServiceImpl extends WechatMessag
     @Override
     public Document messageRecive(WechatNormalTextMessageRequest wechatMessage) throws ServiceException {
         LOGGER.debug("receive a WechatReceiveNormalTextMessage request ");
-        
+
         LOGGER.debug("receive a messageRecive request ");
         String toUserName = wechatMessage.getFromUserName();
         String fromUserName = wechatMessage.getToUserName();
@@ -83,15 +95,37 @@ public class CustomerWechatMessageReceiveProcessServiceImpl extends WechatMessag
             }
         }
         if (!continueReplay) {
-            TulingResponse response = new TulingHelper().callTuling(message);
+            final TulingResponse response = new TulingHelper().callTuling(message);
             int tulingCode = response.getCode();
             switch (tulingCode) {
                 case TulingConstants.TULING_RESPONSE_CODE_TEXT:
-                    return replayTextMessage(toUserName, fromUserName,response.getText());
+                    return replayTextMessage(toUserName, fromUserName, response.getText());
                 case TulingConstants.TULING_RESPONSE_CODE_LINK:
-                    //TODO 調用發送模板消息
-                    return replayTextMessage(toUserName, fromUserName,response.getText()+response.getUrl());
-                default: 
+                    //返回圖片需要處理時間，直接返回NULL值，通過異步進行處理發送消息
+                    scheduledThreadPool.submit(() -> {
+                        try {
+                            String url = response.getUrl();
+                            String text = response.getText();
+                            //下載圖片並且上傳到微信上，生成圖文消息
+                            File tmpFile = new HttpService().getFileResponseFromHttpGetMethod(url);
+                            if (tmpFile != null) {
+                                String fileName = tmpFile.getName().toLowerCase();
+                                String fileType = fileName.substring(fileName.lastIndexOf(".") + 1);
+                                if (fileType.contains("jpg") || fileType.contains("jpeg") || fileType.contains("png")) {
+                                    //僅支持的圖片類型
+                                    WechatUploadMediaResponse uploadMediaResponse = WechatMediaHandler.uploadMedia(tmpFile, "image", wechatMediaService.getAccessTokenFromLocal());
+                                    String mediaId = uploadMediaResponse.getMediaId();
+                                    LOGGER.debug("微信臨時圖片素材上傳成功，mediaId=" + mediaId);
+                                }
+
+                            }
+                        } catch (ServiceException ex) {
+                            LOGGER.error("Server Error",ex);
+                        }
+                    });
+              
+                    return replayTextMessage(toUserName, fromUserName, response.getText() + response.getUrl());
+                default:
                     return replayErrorTextMessage(toUserName, fromUserName, message);
             }
         }
@@ -114,14 +148,13 @@ public class CustomerWechatMessageReceiveProcessServiceImpl extends WechatMessag
                 switch (tulingCode) {
                     case TulingConstants.TULING_RESPONSE_CODE_TEXT:
                         return replayTextMessage(toUserName, fromUserName);
-                    default: 
+                    default:
                         return replayErrorTextMessage(toUserName, fromUserName, message);
                 }
             }
         }
 
     }
-    
 
     public ThirdpartyUserService getThirdpartyUserService() {
         return thirdpartyUserService;
@@ -138,6 +171,12 @@ public class CustomerWechatMessageReceiveProcessServiceImpl extends WechatMessag
     public void setWechatUserService(WechatUserService wechatUserService) {
         this.wechatUserService = wechatUserService;
     }
-    
-    
+
+    public WechatMediaService getWechatMediaService() {
+        return wechatMediaService;
+    }
+
+    public void setWechatMediaService(WechatMediaService wechatMediaService) {
+        this.wechatMediaService = wechatMediaService;
+    }
 }
