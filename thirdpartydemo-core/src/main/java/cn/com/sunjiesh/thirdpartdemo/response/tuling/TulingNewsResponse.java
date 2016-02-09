b3e6bbb984a5ff86ce29e1d7903551fee8cfcf87
TomSun <sunjie310110@gmail.com>
2016-02-09 21:58:09 +0800

    resolve compile problem

---
+++ thirdpartydemo-core/src/main/java/cn/com/sunjiesh/thirdpartdemo/response/tuling/TulingNewsResponse.java
@@ -0,0 +1,51 @@
+package cn.com.sunjiesh.thirdpartdemo.response.tuling;
+
+/**
+ * 图灵新闻对象
+ * 
+ * @author tom
+ *
+ */
+public class TulingNewsResponse {
+
+	private String article;
+
+	private String icon;
+
+	private String source;
+
+	private String detailurl;
+
+	public String getIcon() {
+		return icon;
+	}
+
+	public void setIcon(String icon) {
+		this.icon = icon;
+	}
+
+	public String getArticle() {
+		return article;
+	}
+
+	public void setArticle(String article) {
+		this.article = article;
+	}
+
+	public String getSource() {
+		return source;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	public String getDetailurl() {
+		return detailurl;
+	}
+
+	public void setDetailurl(String detailurl) {
+		this.detailurl = detailurl;
+	}
+
+}
