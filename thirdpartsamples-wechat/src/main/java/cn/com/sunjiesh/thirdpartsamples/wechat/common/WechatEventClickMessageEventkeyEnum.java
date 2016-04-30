0752ee30fedd0c303083452ec130426e4d5e758e
Tom Sun <sunjie310110@gmail.com>
2016-04-30 11:33:28 +0800

    迁移至springboot

---
+++ thirdpartsamples-wechat/src/main/java/cn/com/sunjiesh/thirdpartsamples/wechat/common/WechatEventClickMessageEventkeyEnum.java
@@ -0,0 +1,11 @@
+package cn.com.sunjiesh.thirdpartsamples.wechat.common;
+
+public enum WechatEventClickMessageEventkeyEnum {
+
+	GetTextMessage,//接收文本消息
+	GetImageMessage,//接收圖片消息
+	GetVoiceMessage,//接受语音消息
+	GETNEWSMESSAGE1,
+	GETNEWSMESSAGE2,
+	GetTemplateMessage;//模板消息
+}
