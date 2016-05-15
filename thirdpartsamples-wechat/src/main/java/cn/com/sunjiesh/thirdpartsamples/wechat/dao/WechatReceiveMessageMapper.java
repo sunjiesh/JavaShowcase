e650a5cd94ae57ce97d1849ed361f668ad4d3f05
Tom Sun <sunjie310110@gmail.com>
2016-05-15 21:30:33 +0800

    插入微信消息

--- thirdpartsamples-wechat/src/main/java/cn/com/sunjiesh/thirdpartsamples/wechat/dao/WechatReceiveMessageMapper.java
+++ thirdpartsamples-wechat/src/main/java/cn/com/sunjiesh/thirdpartsamples/wechat/dao/WechatReceiveMessageMapper.java
@@ -1,11 +1,10 @@
 package cn.com.sunjiesh.thirdpartsamples.wechat.dao;
 
 import org.apache.ibatis.annotations.Insert;
-import org.springframework.stereotype.Repository;
 
 import cn.com.sunjiesh.thirdpartdemo.model.WechatReceiveMessage;
 
-@Repository
+
 public interface WechatReceiveMessageMapper {
 
 	@Insert("insert into wechat_receive_message (id, from_user, message_content, message_type, status, create_user,create_time, update_user, update_time)values (#{id,jdbcType=BIGINT}, #{fromUser,jdbcType=VARCHAR}, #{messageContent,jdbcType=VARCHAR}, #{messageType,jdbcType=VARCHAR}, #{status,jdbcType=INTEGER}, #{createUser,jdbcType=VARCHAR}, #{createTime,jdbcType=TIMESTAMP}, #{updateUser,jdbcType=VARCHAR}, #{updateTime,jdbcType=TIMESTAMP})")
