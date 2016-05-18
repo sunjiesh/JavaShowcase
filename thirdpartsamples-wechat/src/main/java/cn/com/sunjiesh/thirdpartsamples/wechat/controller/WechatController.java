df1d8447c90333073288c654dbe63b3900004759
Tom Sun <sunjie310110@gmail.com>
2016-05-18 23:00:43 +0800

    还原controller代码

--- thirdpartsamples-wechat/src/main/java/cn/com/sunjiesh/thirdpartsamples/wechat/controller/WechatController.java
+++ thirdpartsamples-wechat/src/main/java/cn/com/sunjiesh/thirdpartsamples/wechat/controller/WechatController.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -34,66 +37,66 @@ public class WechatController extends SpringBaseController{
 	@Autowired
 	private CustomMessageReceiveService messageReceiveService;
 
-//	/**
-//	 * 微信服务GET请求
-//	 * 
-//	 * @param request HttpServletRequest请求
-//	 * @param response HttpServletResponse请求
-//	 * @throws IOException 异常
-//	 */
-//	@RequestMapping(value = "/receive.do", method = RequestMethod.GET)
-//	public void valid(HttpServletRequest request, HttpServletResponse response)
-//			throws IOException {
-//		LOGGER.debug("Receive a get requeset");
-//
-//		// 将token、timestamp、nonce三个参数进行字典序排序
-//		String timestamp = request.getParameter("timestamp");
-//		String echoStr = request.getParameter("echostr");
-//		String nonce = request.getParameter("nonce");
-//		String signature = request.getParameter("signature");
-//		String responseStr = WechatValidHandler.valid(timestamp, echoStr, nonce, signature);
-//		super.responseText(response, responseStr);
-//
-//	}
-//
-//	
-//
-//	/**
-//	 * 微信服务POST请求
-//	 * @param contentType contentType
-//	 * @param request request
-//	 * @param response response
-//	 */
-//	@RequestMapping(value = "/receive.do", method = RequestMethod.POST)
-//	public void receive(@RequestHeader("Content-Type") String contentType,
-//			HttpServletRequest request,HttpServletResponse response) {
-//		LOGGER.debug("Receive a post requeset");
-//		try {
-//			String requestBody = getRequestContent(request);
-//			Map<String,String> queryParams=new HashMap<String,String>();
-//			String requestQueryString=request.getQueryString();
-//			LOGGER.debug("requestQueryString="+requestQueryString);
-//			if(!StringUtils.isEmpty(requestQueryString)){
-//				String[] queryStringArr=requestQueryString.split("&");
-//				for(String queryString:queryStringArr){
-//					if(queryString.contains("=")){
-//						String[] paramKeyAndValue=queryString.split("=");
-//						String paramKey=paramKeyAndValue[0];
-//						String paramValue=paramKeyAndValue[1];
-//						queryParams.put(paramKey, paramValue);
-//					}
-//				}
-//			}
-//			LOGGER.debug("requestPart=" + requestBody);
-//			LOGGER.debug("contentType=" + contentType);
-//			String respXml=messageReceiveService.messageReceive(requestBody,queryParams);
-//			LOGGER.debug("respXml="+respXml);
-//			super.responseXml(response,respXml);
-//		} catch (ServiceException ex) {
-//			LOGGER.error("接收微信消息处理失败", ex);
-//		} catch (AesException e) {
-//			LOGGER.error("接收微信消息处理失败", e);
-//		}
-//	}
+	/**
+	 * 微信服务GET请求
+	 * 
+	 * @param request HttpServletRequest请求
+	 * @param response HttpServletResponse请求
+	 * @throws IOException 异常
+	 */
+	@RequestMapping(value = "/receive.do", method = RequestMethod.GET)
+	public void valid(HttpServletRequest request, HttpServletResponse response)
+			throws IOException {
+		LOGGER.debug("Receive a get requeset");
+
+		// 将token、timestamp、nonce三个参数进行字典序排序
+		String timestamp = request.getParameter("timestamp");
+		String echoStr = request.getParameter("echostr");
+		String nonce = request.getParameter("nonce");
+		String signature = request.getParameter("signature");
+		String responseStr = WechatValidHandler.valid(timestamp, echoStr, nonce, signature);
+		super.responseText(response, responseStr);
+
+	}
+
+	
+
+	/**
+	 * 微信服务POST请求
+	 * @param contentType contentType
+	 * @param request request
+	 * @param response response
+	 */
+	@RequestMapping(value = "/receive.do", method = RequestMethod.POST)
+	public void receive(@RequestHeader("Content-Type") String contentType,
+			HttpServletRequest request,HttpServletResponse response) {
+		LOGGER.debug("Receive a post requeset");
+		try {
+			String requestBody = getRequestContent(request);
+			Map<String,String> queryParams=new HashMap<String,String>();
+			String requestQueryString=request.getQueryString();
+			LOGGER.debug("requestQueryString="+requestQueryString);
+			if(!StringUtils.isEmpty(requestQueryString)){
+				String[] queryStringArr=requestQueryString.split("&");
+				for(String queryString:queryStringArr){
+					if(queryString.contains("=")){
+						String[] paramKeyAndValue=queryString.split("=");
+						String paramKey=paramKeyAndValue[0];
+						String paramValue=paramKeyAndValue[1];
+						queryParams.put(paramKey, paramValue);
+					}
+				}
+			}
+			LOGGER.debug("requestPart=" + requestBody);
+			LOGGER.debug("contentType=" + contentType);
+			String respXml=messageReceiveService.messageReceive(requestBody,queryParams);
+			LOGGER.debug("respXml="+respXml);
+			super.responseXml(response,respXml);
+		} catch (ServiceException ex) {
+			LOGGER.error("接收微信消息处理失败", ex);
+		} catch (AesException e) {
+			LOGGER.error("接收微信消息处理失败", e);
+		}
+	}
 	
 }
