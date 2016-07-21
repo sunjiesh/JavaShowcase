5c80768c60a3fba3b0797d3a977f03d36c612c53
Tom Sun <sunjie310110@gmail.com>
2016-07-21 23:21:38 +0800

    receive message

--- thirdservice-parent/thirdservice-wechat/src/main/java/cn/com/sunjiesh/thirdservice/wechat/controller/WechatController.java
+++ thirdservice-parent/thirdservice-wechat/src/main/java/cn/com/sunjiesh/thirdservice/wechat/controller/WechatController.java
@@ -1,13 +1,24 @@
 package cn.com.sunjiesh.thirdservice.wechat.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.qq.weixin.mp.aes.AesException;
+
 import cn.com.sunjiesh.wechat.handler.WechatValidHandler;
+import cn.com.sunjiesh.xcutils.common.base.ServiceException;
 
 /**
  * Wechat
@@ -32,4 +43,31 @@ public class WechatController {
 		String responseStr = WechatValidHandler.valid(timestamp, echoStr, nonce, signature);
 		return responseStr;
 	}
+	
+	@RequestMapping(value = "/receive.do", method = RequestMethod.POST)
+	public String receive(@RequestHeader("Content-Type") String contentType,
+			HttpServletRequest request,
+			@RequestBody String requestBody) {
+		LOGGER.debug("Receive a post requeset");
+		Map<String,String> queryParams=new HashMap<String,String>();
+		String requestQueryString=request.getQueryString();
+		LOGGER.debug("requestQueryString="+requestQueryString);
+		if(!StringUtils.isEmpty(requestQueryString)){
+			String[] queryStringArr=requestQueryString.split("&");
+			for(String queryString:queryStringArr){
+				if(queryString.contains("=")){
+					String[] paramKeyAndValue=queryString.split("=");
+					String paramKey=paramKeyAndValue[0];
+					String paramValue=paramKeyAndValue[1];
+					queryParams.put(paramKey, paramValue);
+				}
+			}
+		}
+		LOGGER.debug("requestPart=" + requestBody);
+		LOGGER.debug("contentType=" + contentType);
+		//String respXml=messageReceiveService.messageReceive(requestBody,queryParams);
+//			LOGGER.debug("respXml="+respXml);
+//			super.responseXml(response,respXml);
+		return "XXXXX";
+	} 
 }
