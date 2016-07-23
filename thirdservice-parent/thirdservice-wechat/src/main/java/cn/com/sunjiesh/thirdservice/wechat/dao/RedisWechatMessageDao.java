50e7708623ad4ca6fcd178e55af4f6d8fa77661a
Tom Sun <sunjie310110@gmail.com>
2016-07-23 12:10:29 +0800

    update RedisWechatMessageDao

--- thirdservice-parent/thirdservice-wechat/src/main/java/cn/com/sunjiesh/thirdservice/wechat/dao/RedisWechatMessageDao.java
+++ thirdservice-parent/thirdservice-wechat/src/main/java/cn/com/sunjiesh/thirdservice/wechat/dao/RedisWechatMessageDao.java
@@ -5,6 +5,10 @@ import java.util.concurrent.TimeUnit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.StringRedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -17,13 +21,36 @@ public class RedisWechatMessageDao {
 	private StringRedisTemplate stringRedisTemplate;
 	
 	public void save(String key,String value){
-		stringRedisTemplate.opsForValue().set(key, value);
-		LOGGER.debug(key+" in redis is "+stringRedisTemplate.opsForValue().get(key));
+		stringRedisTemplate.execute(new RedisCallback<Long>(){
+
+			@Override
+			public Long doInRedis(RedisConnection connection) throws DataAccessException {
+				StringRedisConnection jedis = (StringRedisConnection)connection;
+				jedis.select(0);
+				jedis.set(key, value);
+				LOGGER.debug(key+" in redis is "+stringRedisTemplate.opsForValue().get(key));
+				return null;
+			}
+			
+		});
+		
 	}
 	
 	public void save(String key,String value,long timeout){
-		stringRedisTemplate.opsForValue().set(key, value,timeout,TimeUnit.HOURS);
-		LOGGER.debug(key+" in redis is "+stringRedisTemplate.opsForValue().get(key));
+		stringRedisTemplate.execute(new RedisCallback<Long>(){
+
+			@Override
+			public Long doInRedis(RedisConnection connection) throws DataAccessException {
+				StringRedisConnection jedis = (StringRedisConnection)connection;
+				jedis.select(0);
+				jedis.set(key, value);
+				jedis.expire(key, timeout*60*60);
+				LOGGER.debug(key+" in redis is "+stringRedisTemplate.opsForValue().get(key));
+				return null;
+			}
+			
+		});
+		
 	}
 	
 	public String get(String key){
