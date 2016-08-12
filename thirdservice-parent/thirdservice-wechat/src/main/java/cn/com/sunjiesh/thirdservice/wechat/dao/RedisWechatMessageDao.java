0b7425ea83aba0426af6e335c95a39c1a0fb6dfe
Tom Sun <sunjie310110@gmail.com>
2016-08-12 22:54:35 +0800

    调试

--- thirdservice-parent/thirdservice-wechat/src/main/java/cn/com/sunjiesh/thirdservice/wechat/dao/RedisWechatMessageDao.java
+++ thirdservice-parent/thirdservice-wechat/src/main/java/cn/com/sunjiesh/thirdservice/wechat/dao/RedisWechatMessageDao.java
@@ -1,7 +1,5 @@
 package cn.com.sunjiesh.thirdservice.wechat.dao;
 
-import java.util.concurrent.TimeUnit;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
