44e92fe5e8ecb73f74fceef88d6093b751252c35
Jack Sun <sunjie_310110@aliyun.com>
2017-09-30 22:36:02 +0800

    add hello world

---
+++ showcase-tensorflow/src/main/java/cn/yanshi123/showcase/tensorflow/App.java
@@ -0,0 +1,14 @@
+package cn.yanshi123.showcase.tensorflow;
+
+import org.tensorflow.TensorFlow;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) throws Exception {
+		System.out.println(TensorFlow.version());
+	}
+
+}
